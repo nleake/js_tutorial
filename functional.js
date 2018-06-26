deec28a8ee16aa7e3de2366400f6e4e0a1bef199
ndleake <nicholas.d.leake@uscis.dhs.gov>
2018-06-26 03:19:18 +0000

    add palindrome inheritance refactor, add reduce and filter to functional js

--- functional.js
+++ functional.js
@@ -21,3 +21,41 @@ function functionalMap(states) {
   return states.map(state => urlify(state));
 }
 console.log(functionalMap(states));
+
+// filter: Imperative version
+function imperativeFilter(states) {
+    let singleWordStates = [];
+    states.forEach(function(state) {
+      if (state.split(/\s+/).length === 1) {
+        singleWordStates.push(state);
+      }
+    });
+    return singleWordStates;
+  }
+console.log(imperativeFilter(states));
+  
+// filter: Functional version
+function functionalFilter(states) {
+return states.filter(state => state.split(/\s+/).length === 1);
+}
+console.log(functionalFilter(states));
+
+
+// reduce object: Imperative solution
+function imperativeLengths(states) {
+    let lengths = {};
+    states.forEach(function(state) {
+      lengths[state] = state.length;
+    });
+    return lengths;
+  }
+  console.log(imperativeLengths(states));
+  
+  // reduce object: Functional solution
+  function functionalLengths(states) {
+    return states.reduce((lengths, state) => {
+                            lengths[state] = state.length;
+                            return lengths;
+                          }, {});
+  }
+  console.log(functionalLengths(states));
